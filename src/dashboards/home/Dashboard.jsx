7127943ec6426ff9eee0c999b746ff37fac2adef
josushiman <timurmustafa1989@gmail.com>
2024-01-01 21:01:37 +0000

    POC for calling out to create chart stuff

--- src/dashboards/home/Dashboard.jsx
+++ src/dashboards/home/Dashboard.jsx
@@ -10,9 +10,13 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
+  BarChart,
+  Bar,
+  Rectangle,
 } from "recharts";
+import useAsync from "../../hooks/useAsync";
 
-const data = [
+const graphData = [
   {
     name: "Page A",
     uv: 4000,
@@ -57,33 +61,85 @@ const data = [
   },
 ];
 
-const Dashboard = () => (
-  <>
-    <Welcome />
-    <Card>
-      <CardHeader title="Welcome to the administration" />
-      <CardContent>Lorem ipsum sic dolor amet...</CardContent>
-    </Card>
-    {/* https://recharts.org/en-US/examples/LineChartWithXAxisPadding */}
-    <LineChart width={500} height={300} data={data}>
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" padding={{ left: 30, right: 30 }} />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Line
-        type="monotone"
-        dataKey="pv"
-        stroke="#8884d8"
-        activeDot={{ r: 8 }}
-      />
-      <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-    </LineChart>
-    {/* <DirectDebits /> */}
-    {/* <ResourceContextProvider value="admin/country">
-      <CountryList />
-    </ResourceContextProvider> */}
-  </>
-);
+// POC for making async calls with react-admin outside of normal calls.
+// Need to create separate components for each of these, and a routing hook.
+const fetchData = async () => {
+  const response = await fetch(
+    "http://127.0.0.1:8000/ynab/transaction-by-months?months=3"
+  );
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return response.json();
+};
+
+const Dashboard = () => {
+  const { data, loading, error } = useAsync(fetchData);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <>
+      <Welcome />
+      <Card>
+        <CardHeader title="Welcome to the administration" />
+        <CardContent>Lorem ipsum sic dolor amet...</CardContent>
+      </Card>
+      {/* https://recharts.org/en-US/examples/LineChartWithXAxisPadding */}
+      {data && "Loaded"}
+      <LineChart width={500} height={300} data={data.data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="month_long" padding={{ left: 30, right: 30 }} />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey="total_spent"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
+        <Line type="monotone" dataKey="total_earned" stroke="#82ca9d" />
+      </LineChart>
+      <BarChart
+        width={500}
+        height={300}
+        data={data.data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="month_long" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar
+          dataKey="total_earned"
+          fill="#8884d8"
+          activeBar={<Rectangle fill="pink" stroke="blue" />}
+        />
+        <Bar
+          dataKey="total_spent"
+          fill="#82ca9d"
+          activeBar={<Rectangle fill="gold" stroke="purple" />}
+        />
+      </BarChart>
+      {/* <DirectDebits /> */}
+      {/* <ResourceContextProvider value="admin/country">
+        <CountryList />
+      </ResourceContextProvider> */}
+    </>
+  );
+};
 
 export default Dashboard;
---
+++ src/hooks/useAsync.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+const useAsync = (asyncFunction) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const result = await asyncFunction();
+        setData(result);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [asyncFunction]);
+
+  return { data, loading, error };
+};
+
+export default useAsync;
