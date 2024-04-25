b76076c15c5dfca3bc9c5e282faca3ce87f94eac
josushiman <timurmustafa1989@gmail.com>
2024-04-25 23:12:43 +0100

    remove edit on insurance details screen

--- src/pages/loans-and-renewals/insurance/InsuranceDetails.jsx
+++ src/pages/loans-and-renewals/insurance/InsuranceDetails.jsx
@@ -18,63 +18,55 @@ const InsuranceDetails = () => {
       <Navigation />
       {data.map((value, index) => {
         return (
-          <Link
+          <CustomCard
             key={index}
-            to={`/portal/admin/loans-and-renewals/${value.id}`}
-            style={{
-              textDecoration: "none",
-              color: "inherit",
+            sx={{
+              padding: "1.5rem 2rem",
             }}
           >
-            <CustomCard
-              sx={{
-                padding: "1.5rem 2rem",
-              }}
-            >
-              <Typography variant="h6" fontWeight={300}>
-                {value.name}
-              </Typography>
-              <hr
-                style={{ width: "100%", opacity: "25%", marginBottom: "1rem" }}
-              />
-              <Grid container flexDirection={"column"} rowGap={"0.5rem"}>
-                <Grid container justifyContent={"space-between"}>
-                  <Typography>Provider</Typography>
-                  <Typography fontWeight={200}>{value.provider}</Typography>
-                </Grid>
-                <Grid container justifyContent={"space-between"}>
-                  <Typography>Period</Typography>
-                  <Typography fontWeight={200}>{value.period}</Typography>
-                </Grid>
-                <Grid container justifyContent={"space-between"}>
-                  <Typography>Amount paid</Typography>
-                  <Typography fontWeight={200}>
-                    £ {formatCurrency(value.payment_amount, false, true)}
-                  </Typography>
-                </Grid>
-                <Grid container justifyContent={"space-between"}>
-                  <Typography>Start date</Typography>
-                  <Typography fontWeight={200}>{value.start_date}</Typography>
-                </Grid>
-                <Grid container justifyContent={"space-between"}>
-                  <Typography>End date</Typography>
-                  <Typography fontWeight={200}>{value.end_date}</Typography>
-                </Grid>
-                <Grid container justifyContent={"space-between"}>
-                  <Typography>Link to account</Typography>
-                  {value.notes ? (
-                    <Link to={value.notes} target="_blank" fontWeight={200}>
-                      LINK
-                    </Link>
-                  ) : (
-                    <Typography fontWeight={200}>
-                      <em>N/A</em>
-                    </Typography>
-                  )}
-                </Grid>
+            <Typography variant="h6" fontWeight={300}>
+              {value.name}
+            </Typography>
+            <hr
+              style={{ width: "100%", opacity: "25%", marginBottom: "1rem" }}
+            />
+            <Grid container flexDirection={"column"} rowGap={"0.5rem"}>
+              <Grid container justifyContent={"space-between"}>
+                <Typography>Provider</Typography>
+                <Typography fontWeight={200}>{value.provider}</Typography>
               </Grid>
-            </CustomCard>
-          </Link>
+              <Grid container justifyContent={"space-between"}>
+                <Typography>Period</Typography>
+                <Typography fontWeight={200}>{value.period}</Typography>
+              </Grid>
+              <Grid container justifyContent={"space-between"}>
+                <Typography>Amount paid</Typography>
+                <Typography fontWeight={200}>
+                  £ {formatCurrency(value.payment_amount, false, true)}
+                </Typography>
+              </Grid>
+              <Grid container justifyContent={"space-between"}>
+                <Typography>Start date</Typography>
+                <Typography fontWeight={200}>{value.start_date}</Typography>
+              </Grid>
+              <Grid container justifyContent={"space-between"}>
+                <Typography>End date</Typography>
+                <Typography fontWeight={200}>{value.end_date}</Typography>
+              </Grid>
+              <Grid container justifyContent={"space-between"}>
+                <Typography>Link to account</Typography>
+                {value.notes ? (
+                  <Link to={value.notes} target="_blank" fontWeight={200}>
+                    LINK
+                  </Link>
+                ) : (
+                  <Typography fontWeight={200}>
+                    <em>N/A</em>
+                  </Typography>
+                )}
+              </Grid>
+            </Grid>
+          </CustomCard>
         );
       })}
     </Grid>
