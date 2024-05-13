081d3ba298ec82f9076283a29877b54470d17bc4
josushiman <timurmustafa1989@gmail.com>
2024-05-13 20:43:12 +0100

    made stuff async

--- src/dataProvider.js
+++ src/dataProvider.js
@@ -1,13 +1,19 @@
 import { fetchUtils } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 
-const httpClient = (url, options = {}) => {
+const httpClient = async (url, options = {}) => {
   if (!options.headers) {
     options.headers = new Headers({ Accept: "application/json" });
   }
 
   options.headers.set("x-token", import.meta.env.VITE_JSON_SERVER_TOKEN);
-  return fetchUtils.fetchJson(url, options);
+  const { status, headers, body, json } = await fetchUtils.fetchJson(
+    url,
+    options
+  );
+
+  // console.log("fetchJson result", { status, headers, body, json });
+  return { status, headers, body, json };
 };
 
 export const dataProvider = jsonServerProvider(
