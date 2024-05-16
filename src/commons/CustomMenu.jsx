762ad60d7b046cc14f7d2fa2fde4e1ef3bce7018
josushiman <timurmustafa1989@gmail.com>
2024-05-16 22:19:44 +0100

    logout button on side menu

--- src/commons/CustomMenu.jsx
+++ src/commons/CustomMenu.jsx
@@ -1,23 +1,45 @@
-import { Menu, UserMenu } from "react-admin";
+import { Menu, useLogout } from "react-admin";
+import Grid from "@mui/material/Unstable_Grid2";
 import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import LogoutIcon from "@mui/icons-material/Logout";
 import MoneyIcon from "@mui/icons-material/Money";
 
-const CustomMenu = () => (
-  <Menu>
-    <Menu.DashboardItem primaryText="Finances" leftIcon={<MoneyIcon />} />
+const LogoutMenuButton = ({ ref }) => {
+  const logout = useLogout();
+  const handleClick = () => logout();
+
+  return (
     <Menu.Item
-      to="/gym-summary"
-      primaryText="Gym Summary"
-      leftIcon={<FitnessCenterIcon />}
+      to="/"
+      primaryText="Logout"
+      leftIcon={<LogoutIcon />}
+      onClick={handleClick}
+      ref={ref}
     />
-    <hr style={{ width: "100%" }} />
-    <Menu.ResourceItem name="portal/admin/budgets" />
-    <Menu.ResourceItem name="portal/admin/ynab-categories" />
-    <Menu.ResourceItem name="portal/admin/loans-and-renewals" />
-    <Menu.ResourceItem name="portal/admin/savings" />
-    <Menu.ResourceItem name="portal/admin/ynab-server-knowledge" />
-    <Menu.ResourceItem name="portal/admin/ynab-transaction" />
-    <UserMenu />
+  );
+};
+
+const CustomMenu = () => (
+  <Menu sx={{ backgroundColor: "#121212", height: "100vh" }}>
+    <Grid container>
+      <Grid container>
+        <Menu.DashboardItem primaryText="Finances" leftIcon={<MoneyIcon />} />
+        <Menu.Item
+          to="/gym-summary"
+          primaryText="Gym Summary"
+          leftIcon={<FitnessCenterIcon />}
+        />
+        <hr style={{ width: "100%" }} />
+        <Menu.ResourceItem name="portal/admin/budgets" />
+        <Menu.ResourceItem name="portal/admin/ynab-categories" />
+        <Menu.ResourceItem name="portal/admin/loans-and-renewals" />
+        <Menu.ResourceItem name="portal/admin/savings" />
+        <Menu.ResourceItem name="portal/admin/ynab-server-knowledge" />
+        <Menu.ResourceItem name="portal/admin/ynab-transaction" />
+      </Grid>
+      <hr style={{ width: "100%" }} />
+      <LogoutMenuButton />
+    </Grid>
   </Menu>
 );
 
