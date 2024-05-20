f85f84959c672fbd29dfab9715ed18283e61811c
josushiman <timurmustafa1989@gmail.com>
2024-05-20 23:49:53 +0100

    better graph tooltip stuff

--- src/dashboards/month-summary/components/AverageCardBill.jsx
+++ src/dashboards/month-summary/components/AverageCardBill.jsx
@@ -1,12 +1,15 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
+import { useState } from "react";
 import useAsync from "../../../hooks/useAsync";
 import HandleDataLoad from "../../../commons/HandleDataLoad";
 import { CustomCard } from "../../../commons/CustomCard";
 import AverageCardBillChart from "./AverageCardBillChart";
+import formatCurrency from "../../../hooks/formatCurrency";
 
 const AverageCardBill = () => {
   const { data, loading, error } = useAsync("/average-card-bill?months=6");
+  const [selectedItem, setSelectedItem] = useState(undefined);
 
   if (loading || !data || error) {
     return <HandleDataLoad data={data} loading={loading} error={error} />;
@@ -28,10 +31,62 @@ const AverageCardBill = () => {
       <hr style={{ width: "100%", opacity: "25%", marginBottom: "1rem" }} />
       <Grid container flexDirection={"column"} rowGap={"0.5rem"}>
         {billsLength > 0 ? (
-          <AverageCardBillChart data={data} />
+          <AverageCardBillChart data={data} setSelectedItem={setSelectedItem} />
         ) : (
           <Typography>No card bill data.</Typography>
         )}
+        <Grid
+          container
+          display={"grid"}
+          gridTemplateColumns={"auto 1fr"}
+          columnGap={"0.5rem"}
+        >
+          <Box border={"1px solid #313131"} borderRadius={"0.25rem"}>
+            <Grid
+              container
+              flexDirection={"column"}
+              flexWrap={"nowrap"}
+              rowGap={"0.5rem"}
+              padding={"0.25rem 1rem"}
+            >
+              <Typography color={"#BA306A"}>◆ AMEX</Typography>
+              <Typography color={"#CFB09A"}>◆ HSBC</Typography>
+              <Typography color={"#F0F0C9"}>◆ Barclays</Typography>
+            </Grid>
+          </Box>
+          <Box>
+            {selectedItem ? (
+              <Grid
+                container
+                flexDirection={"column"}
+                flexWrap={"nowrap"}
+                rowGap={"0.5rem"}
+                padding={"0.25rem 1rem"}
+                alignItems={"flex-end"}
+              >
+                <Typography>
+                  £ {formatCurrency(selectedItem["BA AMEX"])}
+                </Typography>
+                <Typography>
+                  £ {formatCurrency(selectedItem["HSBC CC"])}
+                </Typography>
+                <Typography>
+                  £ {formatCurrency(selectedItem["Barclays CC"])}
+                </Typography>
+              </Grid>
+            ) : (
+              <Grid
+                container
+                width={"100%"}
+                height={"100%"}
+                alignContent={"center"}
+                justifyContent={"center"}
+              >
+                <Typography fontStyle={"italic"}>Select a month</Typography>
+              </Grid>
+            )}
+          </Box>
+        </Grid>
       </Grid>
     </CustomCard>
   );
--- src/dashboards/month-summary/components/AverageCardBillChart.jsx
+++ src/dashboards/month-summary/components/AverageCardBillChart.jsx
@@ -6,139 +6,81 @@ import {
   ResponsiveContainer,
   Tooltip,
 } from "recharts";
-import { Box, Typography } from "@mui/material";
-import Grid from "@mui/material/Unstable_Grid2";
 import dayjs from "dayjs";
 import formatCurrency from "../../../hooks/formatCurrency";
-import { useState } from "react";
 
-const CustomTooltip = ({ active, payload, setSelectedItem }) => {
-  if (active && payload && payload.length) {
-    return setSelectedItem(payload[0].payload);
-  }
+const AverageCardBillChart = ({ data, setSelectedItem }) => {
+  const handleMouseMove = (e) => {
+    if (e.activePayload) {
+      setSelectedItem(e.activePayload[0].payload);
+    } else {
+      setSelectedItem(undefined);
+    }
+  };
 
-  return null;
-};
-
-const AverageCardBillChart = ({ data }) => {
-  const [selectedItem, setSelectedItem] = useState(undefined);
   return (
-    <>
-      <ResponsiveContainer width="100%" height={150}>
-        <LineChart
-          data={data}
-          margin={{
-            top: 0,
-            right: 0,
-            left: 0,
-            bottom: 0,
-          }}
-        >
-          <XAxis
-            dataKey="date"
-            stroke="white"
-            tickMargin={10}
-            tickFormatter={(value) => {
-              let dateTime = dayjs(value);
-              return dateTime.format("MMM");
-            }}
-          />
-          <YAxis
-            stroke="white"
-            tickMargin={10}
-            domain={["dataMin", "dataMax + 10"]}
-            tickFormatter={(value) => {
-              let axisTick = formatCurrency(value, true);
-              if (value >= 1000) {
-                let axisParts = axisTick.split(",");
-                return axisParts[0] + "k";
-              }
-              return axisTick;
-            }}
-          />
-          <Tooltip
-            content={<CustomTooltip />}
-            setSelectedItem={setSelectedItem}
-          />
-          <Line
-            type="bump"
-            dataKey="BA AMEX"
-            dot={true}
-            stroke="#BA306A"
-            strokeWidth={2}
-            strokeOpacity={0.7}
-          />
-          <Line
-            type="bump"
-            dataKey="HSBC CC"
-            dot={false}
-            stroke="#CFB09A"
-            strokeWidth={2}
-            strokeOpacity={0.7}
-          />
-          <Line
-            type="bump"
-            dataKey="Barclays CC"
-            dot={false}
-            stroke="#F0F0C9"
-            strokeWidth={2}
-            strokeOpacity={0.7}
-          />
-        </LineChart>
-      </ResponsiveContainer>
-      <Grid
-        container
-        display={"grid"}
-        gridTemplateColumns={"auto 1fr"}
-        columnGap={"0.5rem"}
+    <ResponsiveContainer width="100%" height={150}>
+      <LineChart
+        data={data}
+        margin={{
+          top: 5,
+          right: 0,
+          left: 0,
+          bottom: 0,
+        }}
+        onClick={(e) => handleMouseMove(e)}
+        onMouseLeave={() => setSelectedItem(undefined)}
+        // onTouchMove={(e) => handleMouseMove(e)}
       >
-        <Box border={"1px solid #313131"} borderRadius={"0.25rem"}>
-          <Grid
-            container
-            flexDirection={"column"}
-            flexWrap={"nowrap"}
-            rowGap={"0.5rem"}
-            padding={"0.25rem 1rem"}
-          >
-            <Typography color={"#BA306A"}>◆ AMEX</Typography>
-            <Typography color={"#CFB09A"}>◆ HSBC</Typography>
-            <Typography color={"#F0F0C9"}>◆ Barclays</Typography>
-          </Grid>
-        </Box>
-        <Box>
-          {selectedItem ? (
-            <Grid
-              container
-              flexDirection={"column"}
-              flexWrap={"nowrap"}
-              rowGap={"0.5rem"}
-              padding={"0.25rem 1rem"}
-              alignItems={"flex-end"}
-            >
-              <Typography>
-                £ {formatCurrency(selectedItem["BA AMEX"])}
-              </Typography>
-              <Typography>
-                £ {formatCurrency(selectedItem["HSBC CC"])}
-              </Typography>
-              <Typography>
-                £ {formatCurrency(selectedItem["Barclays CC"])}
-              </Typography>
-            </Grid>
-          ) : (
-            <Grid
-              container
-              width={"100%"}
-              height={"100%"}
-              alignContent={"center"}
-              justifyContent={"center"}
-            >
-              <Typography fontStyle={"italic"}>Select a month</Typography>
-            </Grid>
-          )}
-        </Box>
-      </Grid>
-    </>
+        <XAxis
+          dataKey="date"
+          stroke="white"
+          tickMargin={10}
+          tickFormatter={(value) => {
+            let dateTime = dayjs(value);
+            return dateTime.format("MMM");
+          }}
+        />
+        <YAxis
+          stroke="white"
+          tickMargin={10}
+          domain={["dataMin", "dataMax + 10"]}
+          tickFormatter={(value) => {
+            let axisTick = formatCurrency(value, true);
+            if (value >= 1000) {
+              let axisParts = axisTick.split(",");
+              return axisParts[0] + "k";
+            }
+            return axisTick;
+          }}
+        />
+        <Tooltip content={<></>} />
+        <Line
+          type="bump"
+          dataKey="BA AMEX"
+          dot={true}
+          stroke="#BA306A"
+          strokeWidth={2}
+          strokeOpacity={0.7}
+        />
+        <Line
+          type="bump"
+          dataKey="HSBC CC"
+          dot={false}
+          stroke="#CFB09A"
+          strokeWidth={2}
+          strokeOpacity={0.7}
+        />
+        <Line
+          type="bump"
+          dataKey="Barclays CC"
+          dot={false}
+          stroke="#F0F0C9"
+          strokeWidth={2}
+          strokeOpacity={0.7}
+        />
+      </LineChart>
+    </ResponsiveContainer>
   );
 };
 
