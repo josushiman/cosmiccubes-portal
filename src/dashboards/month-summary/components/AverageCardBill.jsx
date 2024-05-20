e87626204e09fdb6416c61b504a1a1970c4cefd9
josushiman <timurmustafa1989@gmail.com>
2024-05-20 23:04:35 +0100

    no card bills support

--- src/dashboards/month-summary/components/AverageCardBill.jsx
+++ src/dashboards/month-summary/components/AverageCardBill.jsx
@@ -6,7 +6,7 @@ import { CustomCard } from "../../../commons/CustomCard";
 import AverageCardBillChart from "./AverageCardBillChart";
 
 const AverageCardBill = () => {
-  const { data, loading, error } = useAsync("/average-card-bill");
+  const { data, loading, error } = useAsync("/average-card-bill?months=6");
 
   if (loading || !data || error) {
     return <HandleDataLoad data={data} loading={loading} error={error} />;
@@ -27,8 +27,11 @@ const AverageCardBill = () => {
       </Grid>
       <hr style={{ width: "100%", opacity: "25%", marginBottom: "1rem" }} />
       <Grid container flexDirection={"column"} rowGap={"0.5rem"}>
-        {billsLength > 0 && <AverageCardBillChart data={data} />}
-        {/* TODO add result when no results returned */}
+        {billsLength > 0 ? (
+          <AverageCardBillChart data={data} />
+        ) : (
+          <Typography>No card bill data.</Typography>
+        )}
       </Grid>
     </CustomCard>
   );
